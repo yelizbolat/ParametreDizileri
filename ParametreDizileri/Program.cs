199b1106f145f40dc2325dd6c5c707def85acf72
bolat <bolat@YELIZ>
2023-12-05 09:55:12 +0300

    Proje dosyası ekle.

---
+++ ParametreDizileri/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametreDizileri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayiIslemleri si = new SayiIslemleri();
+            Console.WriteLine("Toplam: {0}", si.Toplam(3));
+            Console.WriteLine("Toplam: {0}",si.Toplam(3,4,5));
+            Console.WriteLine("Toplam: {0}", si.Toplam(5, 1, 7, 3, 4, 5));
+        }
+    }
+    class SayiIslemleri
+    {
+        public int Toplam(params int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (var s in  sayilar)
+            {
+                toplam += s;
+            }
+            return toplam;
+        }
+    }
+}
